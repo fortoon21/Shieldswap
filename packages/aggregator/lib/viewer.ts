673cad44bdd2de3743c32b0b2722f097a1cb0567
Taiju Sanagi <taijusanagi@gmail.com>
2022-08-10 18:59:00 +0900

    test is working without implementation

--- packages/aggregator/lib/viewer.ts
+++ packages/aggregator/lib/viewer.ts
@@ -2,7 +2,8 @@ import { ethers } from "ethers";
 import { Contract, Provider } from "ethers-multicall";
 
 import UniswapV2ViewerArtifact from "../artifacts/contracts/implements/UniswapV2Viewer.sol/UniswapV2Viewer.json";
-import { UniswapV2Viewer } from "../typechain-types";
+import UniswapV3ViewerArtifact from "../artifacts/contracts/implements/UniswapV3Viewer.sol/UniswapV3Viewer.json";
+import { UniswapV2Viewer, UniswapV3Viewer } from "../typechain-types";
 import { logger } from "./logger";
 
 export class ViewerLib {
@@ -152,5 +153,94 @@ export class UniswapV2ViewerLib extends ViewerLib {
       return { token: v.token, decimals: v.decimals, name: v.name, symbol: v.symbol };
     });
     return result;
+  };
+}
+
+export class UniswapV3ViewerLib extends ViewerLib {
+  contract: UniswapV3Viewer;
+  multicallContract: Contract;
+
+  constructor(
+    prvider: ethers.providers.JsonRpcProvider,
+    address: string,
+    multicallChunkLength: number,
+    chunckedMulticallConcurrency: number,
+    limit?: number
+  ) {
+    super(prvider, multicallChunkLength, chunckedMulticallConcurrency, limit);
+    this.contract = new ethers.Contract(address, UniswapV3ViewerArtifact.abi, prvider) as UniswapV3Viewer;
+    this.multicallContract = new Contract(address, UniswapV3ViewerArtifact.abi);
+  }
+
+  getPools = async () => {
+    // const poolLength = await this.contract.getPoolsLength();
+    // const chunkedMulticall = await this.createChunkedMulticall(
+    //   poolLength.toNumber(),
+    //   this.multicallContract.getPoolAddressByIndex,
+    //   "index"
+    // );
+    // return await this.processChunkedMulticall(chunkedMulticall);
+    return [];
+  };
+
+  getPoolInfosByPools = async (pools: string[]) => {
+    // if (pools.length === 0) {
+    //   return [];
+    // }
+    // const chunkedMulticall = await this.createChunkedMulticall(pools.length, this.multicallContract.getPoolInfo, pools);
+    // let result = await this.processChunkedMulticall(chunkedMulticall);
+    // result = result.filter((PoolInfo) => {
+    //   return (
+    //     /*
+    //      * @dev: remove one of token balance is zero
+    //      */
+    //     ethers.BigNumber.from(PoolInfo.tokenBalances[0]).gt(0) && ethers.BigNumber.from(PoolInfo.tokenBalances[0]).gt(1)
+    //   );
+    // });
+    // /*
+    //  * @dev: make it object array for export
+    //  */
+    // result = result.map((v) => {
+    //   return {
+    //     totalSupply: v.totalSupply,
+    //     tokenBalances: v.tokenBalances,
+    //     pool: v.pool,
+    //     tokenList: v.tokenList,
+    //     fees: v.fees,
+    //     decimals: v.decimals,
+    //     name: v.name,
+    //     symbol: v.symbol,
+    //   };
+    // });
+    // return result;
+    return [];
+  };
+
+  getTokensByPoolInfos = async (poolInfos: any[]) => {
+    // if (poolInfos.length === 0) {
+    //   return [];
+    // }
+    // let tokenList = poolInfos.map((poolInfo) => poolInfo.tokenList).flat();
+    // tokenList = [...new Set(tokenList)];
+    // const chunkedMulticall = await this.createChunkedMulticall(
+    //   tokenList.length,
+    //   this.multicallContract.tokenInfo,
+    //   tokenList
+    // );
+    // let result = await this.processChunkedMulticall(chunkedMulticall);
+    // result = result.filter((token) => {
+    //   /*
+    //    * @dev: remove token name and symbol is invalid
+    //    */
+    //   return token.name !== "" && token.string !== "";
+    // });
+    // /*
+    //  * @dev: make it object array for export
+    //  */
+    // result = result.map((v) => {
+    //   return { token: v.token, decimals: v.decimals, name: v.name, symbol: v.symbol };
+    // });
+    // return result;
+    return [];
   };
 }
