8246a660620bb71e49fea0248f3e68c445ab6f0d
Taiju Sanagi <taijusanagi@gmail.com>
2022-08-11 23:40:01 +0900

    forgot to update fornt

--- packages/frontend/src/lib/app/constants.ts
+++ packages/frontend/src/lib/app/constants.ts
@@ -3,5 +3,5 @@ export const SERVICE_DESCRIPTION = "MEV Resistant Flashloanable DEX Aggregator";
 export const SERVICE_BASE_URL = "https://2022-eth-seoul-submission.vercel.app";
 
 // TODO: this is not DRY code, update later
-export const APPROVE_CONTRACT_ADDRESS = "0x235e7ABe334873df1f7ab4dc774C8fDc93E0a007";
-export const ROUTER_CONTRACT_ADDRESS = "0xF563f64e50C8989656995E7593A0d79f5e4Cb80F";
+export const APPROVE_CONTRACT_ADDRESS = "0x827f91Ee77fF96b5ac95Ac20C8335774F2dbdb9F";
+export const ROUTER_CONTRACT_ADDRESS = "0xE8cb4b4b47bFF4c12a599FdB13b751C4f41974Ff";
